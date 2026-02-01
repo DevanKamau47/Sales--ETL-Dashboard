3f32572d39154277eca8ad0c92e727f6d7e698d7
Devan Kamau <devan.kiragu@strathmore.edu>
2026-02-01 21:40:02 +0300

    Data Loading and Transformation

--- etl/load.py
+++ etl/load.py
@@ -1,5 +1,14 @@
 from sqlalchemy import create_engine
 
 def load_data(df):
-    engine = create_engine("postgresql://user:password@localhost:5432/salesdb")
-    df.to_sql("sales", engine, if_exists="replace", index=False)
+    engine = create_engine(
+        "postgresql://postgres:12345@localhost:5432/salesdb"
+    )
+
+    df.to_sql(
+        "sales",
+        engine,
+        if_exists="replace",
+        index=False
+    )
+
--- etl/transform.py
+++ etl/transform.py
@@ -1,7 +1,14 @@
 import pandas as pd
 
 def transform_data(df):
+    # standardize column names
     df.columns = df.columns.str.lower().str.replace(" ", "_")
-    df['order_date'] = pd.to_datetime(df['order_date'])
+
+    # FIX: explicitly tell pandas day comes first
+    df['order_date'] = pd.to_datetime(df['order_date'], dayfirst=True)
+
+    # remove missing values
     df = df.dropna()
+
     return df
+
